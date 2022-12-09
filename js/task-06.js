e8c70d16bbc88ab398f574eaf7c099e0332efd72
RomanSyhlianyk <romansyglianyk@gmail.com>
2022-12-09 19:24:46 +0200

    Виправив помилки

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,24 @@
 const validationInputEl = document.querySelector("#validation-input");
 // console.log(validationInputEl.dataset.length)
-validationInputEl.addEventListener('blur', validValue)
-
+validationInputEl.addEventListener("blur", validValue);
+const maxCount = validationInputEl.getAttribute('data-length')
 function validValue(e) {
-    // console.log(e.currentTarget.value.length)
-    // console.log(+e.target.dataset.length)
-    if (+e.target.dataset.length === e.currentTarget.value.length) { e.currentTarget.classList.add("valid") }
-    else if (e.currentTarget.value.length === 0) { e.currentTarget.classList.remove("valid"), e.currentTarget.classList.remove("invalid") } else { e.currentTarget.classList.add("invalid")}
-   
+  // console.log(e.currentTarget.value.length)
+  // console.log(+e.target.dataset.length)
+  // if (+e.target.dataset.length !== e.currentTarget.value.length) {
+  //   e.currentTarget.classList.add("invalid");
+  // } else if (+e.target.dataset.length === 0) {
+  //   e.currentTarget.classList.remove("invalid");
+  // } else {
+  //   e.currentTarget.classList.remove("invalid");
+  //   e.currentTarget.classList.add("valid");
+  // }
+  console.log(e.target.value.length);
+  if (e.target.value.length === Number(maxCount)) {
+    validationInputEl.classList.remove("invalid");
+    validationInputEl.classList.add('valid');
+  } else {
+    validationInputEl.classList.remove("valid");
+    validationInputEl.classList.add("invalid");
+  }
 }
--- js/task-09.js
+++ js/task-09.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const widgetEl = document.querySelector(".widget");
+const widgetEl = document.querySelector("body");
 console.log(widgetEl);
 const colorEl = document.querySelector(".color");
 console.log(colorEl);
