f573bc61d7aed8c88b1dc72b5cb89c9760fd7bb5
RomanSyhlianyk <romansyglianyk@gmail.com>
2022-12-09 16:27:35 +0200

    видалив src

---
+++ js/task-09.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const widgetEl = document.querySelector(".widget");
+console.log(widgetEl);
+const colorEl = document.querySelector(".color");
+console.log(colorEl);
+const changeColorEl = document.querySelector(".change-color");
+console.log(changeColorEl);
+
+changeColorEl.addEventListener("click", onchangeColor);
+
+function onchangeColor() {
+  widgetEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+}
